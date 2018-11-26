33d14c1e76fbcd79d32eb14dd5cdb6c56d88e858
Kayla Carrigan <kayla.carrigan@percivaleng.com>
2018-11-26 12:01:50 -0500

    updated test AI

--- ai_test.py
+++ ai_test.py
@@ -20,9 +20,72 @@ trDigits = load_digits()
 def cropper(sourcePath, coords, destPath):
     image1 = Image.open(sourcePath).convert("L")
     cropped = image1.crop(coords)
-    cropped = cropped.resize((8,8), resample=4)
-    load_img(cropped)
+    cropped = cropped.resize((32,32), resample=4) #narrow scope of search 
+    #save image before pixelating
     cropped.save(destPath, 'PNG')
+    
+    close_up(destPath) #clear excess space that would confuse ai
+    
+    
+
+#crops such that the top and bottom rows have pixels
+def close_up(sourcePath):
+    img = Image.open(sourcePath)
+    pixels = img.load()
+    #print (img.size)
+    #print (pixels[0,0])
+    end = 0
+    
+    ####get top
+    for i in range(0,31):
+        for j in range(0,31):
+            if (pixels[j,i] != 255):
+                end = 1
+        if (end == 1):
+            break
+    top = i - 1
+    end = 0
+    a=31;
+    
+    ####get bottom
+    for a in range(31, 0, -1):
+        for b in range(0, 31):
+            if (pixels[b,a] != 255):
+                end = 1
+        if (end == 1):
+            break
+    
+    bottom = a + 1   
+    
+    ####get left
+    for i in range(1,31):
+        for j in range(1,31):
+            if (pixels[i,j] != 255):
+                end = 1
+        if (end == 1):
+            break
+    left = i
+    
+    ####get right
+    for a in range(31, 0, -1):
+        for b in range(0, 31):
+            if (pixels[a,b] != 255):
+                end = 1
+        if (end == 1):
+            break
+    right = a + 1
+    
+    height = bottom - top
+    x1 = (32/2) - (height/2) #from middle x, back to make a square
+    x2 = (32/2) + (height/2) #from middle x, forward to make a square
+    print (top, bottom, x1, x2)
+    img = img.crop((x1,top,x2,bottom))
+    
+    
+    #image1 = Image.open(destPath).convert("L")   
+    cropped = img.resize((8,8), resample=5)
+    load_img(cropped)
+    cropped.save(sourcePath, 'PNG')
 
 #loads an image for processing by the AI
 def load_img(img):
@@ -45,6 +108,7 @@ def load_prelim_data():
             #if (j == 7):
             #    cropper(sourceName, (x, y, x + w - 1, y + h), str(j) + '/s' + str(i) + '_' + str(j) + '.png')    
             #else:
+            
             cropper(sourceName, (x, y, x + w, y + h), str(j) + '/s' + str(i) + '_' + str(j) + '.png')    
             x = x + w + 1 #next number space, plus the pixel border
 
@@ -72,6 +136,7 @@ def main():
 #    pl.gray()
 #    pl.matshow(trDigits.images[0])
 #    print(trDigits.images[0])
+        
     n_samples = len(trDigits.images)
     X_digits = trDigits.data / trDigits.data.max()
     y_digits = trDigits.target
@@ -81,9 +146,9 @@ def main():
     y_test = y_digits[int(.9 * n_samples):]
     classifier = ensemble.RandomForestClassifier()
     classifier.fit(X_digits, y_digits)
-    #print ()
-#    for i in range(10):
-#        print(trDigits.data[i])
+    print ()
+    #for i in range(10):
+        #print(trDigits.data[i])
     for i in range(10):
         pl.matshow(trDigits.images[i])
         print( classifier.predict(arr[i].reshape(1, -1)) )
