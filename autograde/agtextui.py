c97e62ab675baf38160596a483410aa164211c49
Andrew Henry <andrewmichaelhenry@gmail.com>
2018-12-11 11:58:36 -0500

    Start work on UI for demo.

--- autograde/agtextui.py
+++ autograde/agtextui.py
@@ -1,7 +1,21 @@
-def main():
-    """Run AutoGrade UI."""
+from autograde.results import read_results
+
+def generate_report(filename):
+    """
+    TODO: What data exactly do we get from grading results?
+
+    Return a string with the report text.
+    """
     pass
 
+
+def main():
+    """Run AutoGrade UI."""
+    # read arguments
+    import argparse
+
+    # extend to multiple files:
+    print(generate_report(filename))
 
 if __name__ == '__main__':
     main()
