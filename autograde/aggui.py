534c997a15e797e386a8086d9eec71af31b50f75
Andrew Henry <andrewmichaelhenry@gmail.com>
2019-01-27 20:48:31 -0500

    Update GUI.

--- autograde/aggui.py
+++ autograde/aggui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
-from agtextui import generate_report, get_letter_grade
+from agtextui import get_letter_grade
 from results import read_results
 
 """
@@ -11,12 +11,6 @@ https://stackoverflow.com/questions/17466561/best-way-to-structure-a-tkinter-app
 """
 
 EVAL_CONFIDENCE_THRESHOLD = 0.6
-
-def read_file(filename):
-    """Return contents of file."""
-    with open(filename, 'r') as f:
-        source = f.read()
-    return source
 
 
 def get_scores(results):
@@ -33,85 +27,123 @@ def get_fraction_correct(results):
 
 
 SAMPLE_GRADED_OUTPUT = 'saved-results/graded'
-SAMPLE_REPORT = generate_report(SAMPLE_GRADED_OUTPUT)
 
-SAMPLE_RESULTS = read_results(SAMPLE_GRADED_OUTPUT)
+
+SUMMARY_COLUMNS = [
+        dict(name='studentID', title='Student ID', anchor=tk.W),
+        dict(name='scanOrder', title='Scan Order', anchor=tk.E),
+        dict(name='numericalGrade', title='Numerical Grade', anchor=tk.E),
+        dict(name='letterGrade', title='Letter Grade', anchor=tk.CENTER),
+        ]
+
+DETAIL_COLUMNS = [
+        dict(name='question', title='Question', anchor=tk.E),
+        dict(name='score', title='Score', anchor=tk.E),
+        dict(
+            name='evaluationConfidence',
+            title='Evaluation Confidence',
+            anchor=tk.E
+            ),
+        ]
+
+
+class CustomTreeview(ttk.Treeview):
+
+    def __init__(self, parent, columns):
+        """Initialize CustomTreeview widget.
+
+        parent -- the parent widget (as usual)
+        columns -- a list of column descriptor dicts
+
+        """
+        super().__init__(parent)
+
+        self['show'] = 'headings'
+        self['columns'] = [column['name'] for column in columns]
+
+        for column in columns:
+            self.heading(column['name'], text=column['title'])
+            self.column(column['name'], anchor=column['anchor'])
+
+        try:
+            getattr(self, 'columns')
+        except AttributeError:
+            pass
+        else:
+            raise RuntimeError('Treeview already has a `columns` member!')
+
+        self.columns = list(columns)
+
+    def insert(self, parent, index, tags=None, **kwargs):
+        """ """
+        values = [kwargs[column['name']] for column in self.columns]
+        if tags is None:
+            super().insert(parent, index, values=values)
+        else:
+            super().insert(parent, index, tags=tags, values=values)
+
 
 class AutoGradeGui(tk.Frame):
+    """Custom widget to implement AutoGrade GUI."""
 
     def __init__(self, parent, *args, **kwargs):
-        """ """
+        """Initialize AutoGradeGui widget."""
         super().__init__(parent, *args, **kwargs)
 
+        self.student_results = {}
+
         self.summary_label = tk.Label(self, text='Summary')
-        self.summary_label.grid(column=0, row=0, sticky='NSEW')
-
-        self.summary_treeview = ttk.Treeview(self)
-        self.summary_treeview['show'] = 'headings'
-        self.summary_treeview['columns'] = ('studentID', 'numericalGrade', 'letterGrade')
-        self.summary_treeview.heading('#1', text='Student ID')
-        self.summary_treeview.heading('#2', text='Numerical Grade')
-        self.summary_treeview.heading('#3', text='Letter Grade')
-        self.summary_treeview.column('#2', anchor=tk.E)
-        self.summary_treeview.column('#3', anchor=tk.CENTER)
-        self.summary_treeview.grid(column=0, row=1, sticky='NSEW')
-
         self.detail_label = tk.Label(self, text='Details')
-        self.detail_label.grid(column=1, row=0, sticky='NSEW')
+        self.summary_treeview = CustomTreeview(self, SUMMARY_COLUMNS)
+        self.detail_treeview = CustomTreeview(self, DETAIL_COLUMNS)
 
-        self.detail_treeview = ttk.Treeview(self)
-        self.detail_treeview['show'] = 'headings'
-        self.detail_treeview['columns'] = ('question', 'score', 'evaluationConfidence')
-        self.detail_treeview.heading('#1', text='Question')
-        self.detail_treeview.heading('#2', text='Score')
-        self.detail_treeview.heading('#3', text='Evaluation Confidence')
-        self.detail_treeview.column('#1', anchor=tk.E)
-        self.detail_treeview.column('#2', anchor=tk.E)
-        self.detail_treeview.column('#3', anchor=tk.E)
         self.detail_treeview.tag_configure('lowConfidence', foreground='red')
-        self.detail_treeview.grid(column=1, row=1, sticky='NSEW')
 
-        self.textbox = tk.Text(self)
-        self.textbox.config(state='disabled')
-        #self.textbox.grid(column=2, row=1, sticky='NSEW')
+        self.summary_label.grid(column=0, row=0, sticky='NSEW')
+        self.summary_treeview.grid(column=0, row=1, sticky='NSEW')
+        self.detail_label.grid(column=1, row=0, sticky='NSEW')
+        self.detail_treeview.grid(column=1, row=1, sticky='NSEW')
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
-        #self.grid_columnconfigure(2, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=0)
         self.grid_rowconfigure(1, weight=1)
 
     def read_graded_output(self, graded_output):
         """Read information in graded output and add it to GUI."""
-        self.textbox.config(state='normal')
-        self.textbox.delete(1.0, tk.END)
-        self.textbox.insert(tk.END, generate_report(graded_output))
-        self.textbox.config(state='disabled')
-
         results = read_results(graded_output)
-
         studentID = results['name']
-        numericalScore = 100 * get_fraction_correct(results)
-        letterGrade = get_letter_grade(numericalScore)
+        self.student_results[studentID] = results
+
+        numericalGrade = 100 * get_fraction_correct(results)
+        letterGrade = get_letter_grade(numericalGrade)
+        scanOrder = str(len(self.student_results))
 
         self.summary_treeview.insert(
-                '',
-                0,
-                values=(studentID, numericalScore, letterGrade)
+                '', 0,
+                studentID=studentID,
+                numericalGrade=numericalGrade,
+                letterGrade=letterGrade,
+                scanOrder=scanOrder
                 )
 
         for question, data in results['questions'].items():
+
             score = data['score']
             evaluationConfidence = data['evalConf']
+
             if evaluationConfidence < EVAL_CONFIDENCE_THRESHOLD:
                 tags = 'lowConfidence'
             else:
                 tags = ()
+
             self.detail_treeview.insert(
                     '',
                     'end',
-                    values=(question, str(score), str(evaluationConfidence)),
-                    tags=tags
+                    tags=tags,
+                    question=question,
+                    score=str(score),
+                    evaluationConfidence=str(evaluationConfidence),
                     )
 
 
--- autograde/util.py
+++ autograde/util.py
@@ -1,5 +1,12 @@
 import os
 
+def read_file(filename):
+    """Return contents of file."""
+    with open(filename, 'r') as f:
+        source = f.read()
+    return source
+
+
 def get_data_filename(filename):
     """ """
     return os.path.join(os.path.dirname(__file__), 'data', filename)
