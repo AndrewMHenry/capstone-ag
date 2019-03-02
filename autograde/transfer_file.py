809875aa0055e59cf7915e30c1a12d042ef99bb0
yviek <43182768+yviek@users.noreply.github.com>
2019-03-02 12:49:57 -0500

    file transfer code

---
+++ autograde/transfer_file.py
@@ -0,0 +1,12 @@
+from paramiko import SSHClient
+from scp import SCPClient
+ssh = SSHClient()
+
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+target_host = '127.0.0.1'   #replace by address of processing unit
+target_port = 22            # not sure which port number to use
+
+ssh.connect( hostname = target_host , username = 'Capstones', password = 'AutoGrade' )
+with SCPClient(ssh.get_transport()) as scp:
+    scp.put('my_file.txt', 'my_file2.txt') # Copy my_file.txt to the server
+    scp.get('my_file2.txt')  # gets the copy of myfile 
