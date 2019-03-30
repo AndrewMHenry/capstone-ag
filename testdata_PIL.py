703e185b3dfb6d2955fe4470cec4ec6093715981
bam6UMBC <bam6@umbc.edu>
2019-03-30 11:32:53 -0400

    Added python code for iterating through the collected test data sheets as well as the folders containing the 32x32 images it produces.

--- testdata_PIL.py
+++ testdata_PIL.py
@@ -1,4 +1,72 @@
 from PIL import Image
+import pylab as pl
+import numpy as np
+
+def load_img(img):
+    imgarr = np.array(img) 
+    imgarr = np.invert(img) #invert black and white for AI training data
+    #pl.gray() 
+    pl.matshow(imgarr) #uncomment to display
+    return imgarr
+
+#crops such that the top and bottom rows have pixels
+def close_up(sourcePath):
+    img = Image.open(sourcePath)
+    pixels = img.load()
+    #print (img.size)
+    #print (pixels[0,0])
+    end = 0
+    
+    ####get top
+    for i in range(0,31):
+        for j in range(0,31):
+            if (pixels[j,i] != 255):
+                end = 1
+        if (end == 1):
+            break
+    top = i - 1
+    end = 0
+    a=31;
+    
+    ####get bottom
+    for a in range(31, 0, -1):
+        for b in range(0, 31):
+            if (pixels[b,a] != 255):
+                end = 1
+        if (end == 1):
+            break
+    
+    bottom = a + 1   
+    
+    ####get left
+    for i in range(1,31):
+        for j in range(1,31):
+            if (pixels[i,j] != 255):
+                end = 1
+        if (end == 1):
+            break
+    left = i
+    
+    ####get right
+    for a in range(31, 0, -1):
+        for b in range(0, 31):
+            if (pixels[a,b] != 255):
+                end = 1
+        if (end == 1):
+            break
+    right = a + 1
+    
+    height = bottom - top
+    x1 = (32/2) - (height/2) #from middle x, back to make a square
+    x2 = (32/2) + (height/2) #from middle x, forward to make a square
+    #print (top, bottom, x1, x2)
+    img = img.crop((x1,top,x2,bottom))
+    
+    
+    #image1 = Image.open(destPath).convert("L")   
+    cropped = img.resize((8,8), resample=5)
+    load_img(cropped)
+    cropped.save(sourcePath, 'PNG')
 
 # --------------------------------------------------------------------------------------------------------------
 def processPage():
@@ -21,29 +89,32 @@ def processPage():
             topLeftx = 204
             topLefty = 431
 
-        charCtr[digitCtr] += 1
 # --
         imageName = ("data-samples/PNGs/agscan1200_0000-" + str(pageCtr) + ".PNG")
-        imageFile = Image.open(imageName)
+        imageFile = Image.open(imageName).convert("L")
 
         while (boxCtr != 97):
             boxCtr += 1
             digitCtr += 1
 
             if (digitCtr == 12):
-                digitCtr = -1
+                digitCtr = 0
 
-            if (boxCtr > 1) and (boxCtr % 13 != 0):
+            if (boxCtr > 1) and (boxCtr % 12 != 1):
                 topLeftx += 109             # 109px to the right is the next box
-            elif (boxCtr % 13 == 0):
-                topLeftx = 204
+            elif (boxCtr % 12 == 1):
+                topLeftx = 207
                 topLefty = topLefty + 196   # 196px downward is the next row
 
-            box = (topLeftx, topLefty, topLeftx + 105, topLefty + 105)
+# originally +105
+            box = (topLeftx, topLefty, topLeftx + 85, topLefty + 85)
             boxImg = imageFile.crop(box)
             boxImgName = "data-samples/gathered_data/" + chars[digitCtr] + "/s" + chars[digitCtr] + "_" + str(charCtr[digitCtr]) + ".PNG"
-          #  boxImg.resize((32,32), resample=4)
+            boxImg = boxImg.resize((32,32), resample=4)
             boxImg.save(boxImgName, "PNG")
+            close_up(boxImgName) 
+          
+            charCtr[digitCtr] += 1
 
 def main():
     Image.MAX_IMAGE_PIXELS = None
